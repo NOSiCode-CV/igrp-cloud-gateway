00f53f1bd6441e9caa73242cbb4c7c44dc883886
william.fonseca <william.fonseca@nosi.cv>
2025-06-04 00:06:47 +0000

    Enable Discovery Client

--- src/main/java/cv/igrp/platform/igrpcloudgateway/IgrpCloudGatewayApplication.java
+++ src/main/java/cv/igrp/platform/igrpcloudgateway/IgrpCloudGatewayApplication.java
@@ -2,6 +2,7 @@ package cv.igrp.platform.igrpcloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Main application class for IGRP Cloud Gateway.
@@ -12,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Use the 'production' profile for production deployment with Kubernetes.
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class IgrpCloudGatewayApplication {
 
     public static void main(String[] args) {
